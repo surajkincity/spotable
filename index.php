a906564405b596a47a656574204d25b8d46d46df
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-02-02 15:00:18 +0530

    GA

--- index.php
+++ index.php
@@ -340,5 +340,15 @@ info@Spotable.in  &nbsp;&nbsp;&nbsp;  +91 7276-888-089
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-85089212-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
   </body>
 </html>
