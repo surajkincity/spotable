589260a26c5d4c1bd89d1d9b3d2964e02b00c5ae
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-02-05 13:04:50 +0530

    Privacy policy

---
+++ privacy-policy-of-spotable.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <title>Privacy Policy of SPotable</title>
+    <meta name="description" content="We have an experienced team which includes SEO specialists, full stack developers and even Google’s Ex-Employees.">
+    <style type="text/css">
+    #footerstrip{background-color: #3498db}
+    #mainmenu {background-color:#3498db;}
+
+      .aboutp {    color: #95a5a6;
+    font-weight: 400; font-size: 22px;}
+      body{font-family: 'Roboto Condensed',sans-serif !important; background-image: url('img/background1-min.jpg');background-attachment: fixed;}
+    </style>
+    <?PHP include "assets/include/externalfiles.html" ; ?>
+  </head>
+
+  <body>
+
+    <!-- Fixed navbar -->
+
+<?PHP include "assets/include/nav.html" ; ?>
+
+    <!-- Fixed navbar -->
+
+
+    <div id="aboutpage" style="background-color:white;">
+      <div class="container">
+
+
+    <div class="row">
+<br><br><br><br><br><br>
+<h1>Privacy <b>Policy</b></h1>
+
+</div><!--/.nav-collapse -->
+</div>
+</div>
+
+
+
+  <div id="aboutpage" style="background-color:white;">
+    <div class="container">
+
+
+  <div class="row">
+
+
+
+
+<div class="col-md-1"></div>
+
+<div class="col-md-10" id='aboutpagetext'>
+
+<h2>Our Privacy Policy</h2>
+<p>Privacy of our visitors is really important for us. This page will help you understand our privacy policy and the way we handle any data provided by you. In order to keep this document accessible, we have made it available in the footer section of all our pages. </p>
+
+
+  <h2>Data We Collect</h2>
+  <p>This information is applicable to all the data which is being collected by us via our website Spotable.in There are sections on our website where you might have to submit some data which includes the following.</p>
+
+  <p>Name<br>
+  Address ( not always required )<br>
+  Email address<br>
+  Phone number ( not always required )<br></p>
+
+
+  <h2>Use of the Data:</h2>
+  <p>Any email addresses submitted by our visitors are only used to communicate with them and are not shared with any third party. </p>
+
+  <p>We don't send any spam emails to our visitors, However, if you want to receive the blog updates on SEO written by ou team you can subscribe for the same. You can subscribe or unsubscribe from the list at your wish.</p>
+
+
+  <p>We monitor user behavior in order to better design our website and improve the user experience but as valuing your privacy is absolutely important to us thus we don't identify any users personally on the website. Every visitor is anonymous.</p>
+
+  <p>Lastly, Spotable avoids using the personally identifiable information which you provide to us via our website in any way other than the points mentioned above. We also allow our visitors to opt out from any services they have previously opted in for. </p>
+
+  <h2>Data Security</h2>
+  <p>In order to avoid any unauthorized person to access the data of our visitors, We have established all possible security features wherever possible, to prevent it from Physical or Electronic theft.</p>
+
+  <h2>Contact Us</h2>
+  <p>If you have any questions regarding our privacy policies, kindly feel free to contact us from the contact link mentioned in the header of all our pages.</p>
+
+
+
+
+</div>
+<div class="col-md-1"></div>
+</div>
+
+
+  </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?PHP include "assets/include/footmenu.html" ; ?>
