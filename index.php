1679cbaf3db35166f9c43e105c34e1e14ff28722
Suraj Joshi <surajkincity@gmail.comit config --global user.email surajkincity@gmail.com>
2017-02-21 17:27:59 +0530

    23

--- index.php
+++ index.php
@@ -93,7 +93,7 @@ else                /* send the submitted data */
     <div class="container usp" style="padding-bottom: 40px;font-family: 'Roboto Condensed',sans-serif !important;">
 
 <div class='row mt centered'><br>
-<h1 style="text-align:center;font-weight:600;color:black;">Spotable is a SEO company located in Pune (India)</h1>
+<h1 style="text-align:center;font-weight:600;color:black;">Spotable is a SEO company located in Pune, India</h1>
 <p style="font-weight:400;font-size:18px;padding-right:13%;padding-left:13%;">Out motto is to help out small and medium companies in digital marketing, We optimize their websites and make them visible in google search for keywords that are being searched by their customers. </p>
 </div><hr style="border-top: dotted 2px;" />
       <div class='row mt centered'>
